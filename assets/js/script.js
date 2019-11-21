51617c7e212ec93bc29ce7ba5abdece635634e3c
nancyychang <nancy.chang@berkeley.edu>
2019-11-21 15:20:07 -0800

    Made separate script file for home

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,18 +14,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-function scrollWin() {
-  window.scrollBy(0, 550);
-}
-
-var words = [' designer', ' programmer', ' fitness enthusiast', ' animal lover'];
-
-var i = 0;  // the index of the current item to show
-
-setInterval(function() {            // setInterval makes it run repeatedly
-
-    document.getElementById('word').innerHTML = words[i++];    // get the item and increment i to move to the next
-
-    if (i == words.length) i = 0;   // reset to first element if you've reached the end
-}, 3000);                         // 1000 milliseconds == 1 second
